cb4151bc250505a313b9c9435d15cd7004e06a37
Zelda <solis.t.andrea.z@gmail.com>
2025-02-26 10:14:05 -0700

    [AGREGA]un componente para la vista de contacto prevista

---
+++ Agenda_Shuffle/Agenda_Shuffle/componentes/contacto_prevista.swift
@@ -0,0 +1,22 @@
+//
+//  contacto_prevista.swift
+//  Agenda_Shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text("Hola")
+            Text("Gente")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
