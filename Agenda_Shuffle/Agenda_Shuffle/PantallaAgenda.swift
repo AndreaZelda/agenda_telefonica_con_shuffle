c7beb76d0bdf2e544ec2e0614d970972eff05f2d
Zelda <solis.t.andrea.z@gmail.com>
2025-03-05 10:07:19 -0700

    [AGREGA] la funcionalidad e integra la pantalla de agregar contacto

--- Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
+++ Agenda_Shuffle/Agenda_Shuffle/PantallaAgenda.swift
@@ -24,10 +24,19 @@ let contactos = [
 struct PantallaAgenda: View {
     var largo_de_pantalla = UIScreen.main.bounds.width
     var ancho_de_pantalla = UIScreen.main.bounds.height
+    
+    @State var mostrar_pantalla_agregar_contacto: Bool = false
+    
+    @State var contactos_actuales: [ContactoAgenda] = [
+        ContactoAgenda(nombre: "Joana", telefono: "12345"),
+        ContactoAgenda(nombre: "Joana", telefono: "12345"),
+        ContactoAgenda(nombre: "Joana", telefono: "12345"),
+        ContactoAgenda(nombre: "Joana", telefono: "12345")]
+    
     var body: some View {
         ScrollView{
             VStack (spacing: 10){
-                ForEach(contactos){ contacto in
+                ForEach(contactos_actuales){ contacto in
                     //Text("\(contacto.nombre)")
                     ContactoPrevista(contacto_a_mostrar: contacto, al_pulsar:{print("Te envia saludos \(contacto.nombre) desde la pantalla agenda")})
                 }
@@ -52,7 +61,8 @@ struct PantallaAgenda: View {
             }
             .padding(15)
             .onTapGesture {
-                print("Falta implementar esta parte")
+                print("Falta implementar la seccion de agregar contacto")
+                mostrar_pantalla_agregar_contacto.toggle()
             }
             Spacer()
             
@@ -71,7 +81,16 @@ struct PantallaAgenda: View {
             .onTapGesture {
                 print("Lanzar un intent para iniciar la llamada")
             }
-        }
+        }.background(Color.purple)
+            .sheet(isPresented: $mostrar_pantalla_agregar_contacto){
+                PantallaAgregarContacto(boton_salir: {
+                    mostrar_pantalla_agregar_contacto.toggle()
+                },
+                    boton_agregar: {nombre, numero in
+                        let contacto_nuevo = ContactoAgenda(nombre: nombre, telefono: numero)
+                        contactos_actuales.append(contacto_nuevo)
+                })
+            }
     }
 }
 
--- Agenda_Shuffle/Agenda_Shuffle/pantallas/pantalla_agregar_contacto.swift
+++ Agenda_Shuffle/Agenda_Shuffle/pantallas/pantalla_agregar_contacto.swift
@@ -11,6 +11,13 @@ struct PantallaAgregarContacto: View {
     @State private var nombre: String = ""
     @State private var numero_telefonico: String = ""
     
+    var boton_salir: () -> Void = {
+        print("Parece que te has equivocado")
+    }
+    var boton_agregar: (_ nombre: String, _ numero: String) -> Void = {nombre, numero in
+        print("Parece que te has equivocado")
+    }
+    
     var body: some View {
         Text("Colocar la etiqueta de nombre")
         ZStack{
@@ -28,10 +35,19 @@ struct PantallaAgregarContacto: View {
             .padding(10)
         
         HStack{
+            // Este icono es para agregar a un contacto
             Icono(tamaño: 65, ruta_icono: "person.crop.circle.badge.plus", padding: 10)
+                .onTapGesture {
+                    boton_agregar(nombre, numero_telefonico)
+                }
             Spacer()
+            
+            // Este icono es para salir
             Icono(tamaño: 65, ruta_icono: "return")
                 .background(nombre == "" ? Color.red:Color.cyan)
+                .onTapGesture {
+                    boton_salir()
+                }
         }
         .background(Color.cyan)
        
